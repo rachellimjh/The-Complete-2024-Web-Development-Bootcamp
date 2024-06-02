6679feb4b014ab2a8017ac28ea78bccec1dc5a6e
Rachel Lim <rachellimjh@gmail.com>
2024-06-03 07:08:32 +0800

    Simon game

--- Course Modules/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Course Modules/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,10 +1,38 @@
-var buttonColours=["red","blue","green","yellow"];
-var gamePattern=[];
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
 
-function newSequence(){
-    var randomNumber=Math.floor(Math.random()*4);
-    return randomNumber;
+function newSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColour = buttonColours[newSequence()];
+  gamePattern.push(randomNumber);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
 }
 
-var randomChosenColour = buttonColours[newSequence()];
-gamePattern.push(randomChosenColour);
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  //console.log(userClickedPattern);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
