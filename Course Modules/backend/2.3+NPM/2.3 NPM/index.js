ad45d36d9708ab984c0bfd8918d6192615171348
Rachel Lim <rachellimjh@gmail.com>
2024-06-06 22:23:26 +0800

    update

---
+++ Course Modules/backend/2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,10 @@
+// var generateName = require("sillyname");
+
+import generateName from "sillyname";
+var sillyName = generateName();
+console.log("My name is: " + sillyName);
+
+import { randomSuperhero } from "superheroes";
+
+const name = randomSuperhero();
+console.log(`I am ${name}!`);
