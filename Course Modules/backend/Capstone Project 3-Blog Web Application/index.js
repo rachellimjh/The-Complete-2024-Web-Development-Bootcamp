9326853fc002412677184a89e2ee073ce20f97cd
Rachel Lim <rachellimjh@gmail.com>
2024-07-28 08:36:11 +0800

    edit and delete post functionality

--- Course Modules/backend/Capstone Project 3-Blog Web Application/index.js
+++ Course Modules/backend/Capstone Project 3-Blog Web Application/index.js
@@ -4,15 +4,21 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+let posts = [];
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+app.get("/edit-post/:index", (req, res) => {
+  const { index } = req.params;
+  const post = posts[index];
+  res.render("edit.ejs", { index, post });
 });
 
 //implementing the post creation feature
@@ -21,5 +27,21 @@ app.listen(port, () => {
 app.post("/create-post", (req, res) => {
   const { title, date, content } = req.body;
   const newPost = { title, date, content };
-  res.render("index.ejs", { post: newPost });
+  posts.push(newPost);
+  res.render("index.ejs", { posts: posts });
+});
+
+//to edit posts
+app.post("/update-post/:index", (req, res) => {
+  const { index } = req.params;
+  const { title, date, content } = req.body;
+  posts[index] = { title, date, content };
+  res.render("index.ejs", { posts: posts });
+});
+
+//to delete posts
+app.post("/delete-post/:index", (req, res) => {
+  const { index } = req.params;
+  posts.splice(index, 1);
+  res.render("index.ejs", { posts: posts });
 });
