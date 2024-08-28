f779c0e27d0092c2aebde7eb1abe66119a463c5b
Rachel Lim <rachellimjh@gmail.com>
2024-08-29 01:25:15 +0200

    keeper appp part 2

---
+++ Course Modules/backend/302-keeper-app-part-2-start/302-keeper-app-part-2-starting/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => {
+        return <Note title={note.title} content={note.content} />;
+      })}
+      ;
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
